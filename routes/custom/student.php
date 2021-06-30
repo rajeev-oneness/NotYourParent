97005ee3e326a6cdcddd416bf769ce129c669d65
Rajeev Gupta <rrpit9@gmail.com>
2021-06-30 16:59:04 +0530

    Routes

--- routes/custom/student.php
+++ routes/custom/student.php
@@ -1,7 +1,4 @@
 <?php
-namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Route,Auth;
-
 	Route::get('dashboard',function(){
 		return view('student.dashboard');
 	})->name('home');
--- routes/custom/teacher.php
+++ routes/custom/teacher.php
@@ -1,6 +1,4 @@
 <?php
-namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Route,Auth;
 
 	Route::get('dashboard',function(){
 		return view('teacher.dashboard');
