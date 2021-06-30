ea471709bd4c657ac64ad762b3dfe0d3dee60506
DevArpan7 <72331581+Arpan-Chanda@users.noreply.github.com>
2021-06-30 15:52:44 +0530

    Route fixing

--- routes/custom/admin.php
+++ routes/custom/admin.php
@@ -14,17 +14,6 @@ Route::get('dashboard',function(){
 		return view('admin.dashboard');
 	})->name('home');
 
-	// EXAMPLE OF PROPER ROUTING:
-	// Route::group(['prefix' => 'article'], function() {
-	// 	// here goes the article routes
-	// 	Route::get('/')->name('admin.article.list');
-	// 	Route::get('/add')->name('admin.article.add');
-	// 	Route::get('/store')->name('admin.article.store');
-	// 	Route::get('/edit')->name('admin.article.edit');
-	// 	Route::get('/update')->name('admin.article.update');
-	// 	Route::get('/delete')->name('admin.article.delete');
-	// })
-    // Route::get('/user/{id}', [UserController::class, 'show'])
     Route::group(['prefix' => 'article'], function() {
         // here goes the article routes
         Route::get('/',[ArticleController::class, 'index'])->name('admin.article.index');
