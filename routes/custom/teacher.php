4255a70e7b6a6cd7dbe36e8bf2cf825605f64f6b
aamir <aamir@onenesstechs.in>
2021-07-01 13:15:17 +0530

    calendar

--- routes/custom/teacher.php
+++ routes/custom/teacher.php
@@ -23,8 +23,8 @@ Route::get('dashboard',function(){
         Route::post('/add',[TeacherController::class, 'create'])->name('teacher.my-slot.add');
         // Route::post('/store',[CourseController::class, 'store'])->name('teacher.my-course.store');
         // Route::get('/edit/{id}',[CourseController::class, 'edit'])->name('teacher.my-course.edit');
-        // Route::put('/update/{id}',[CourseController::class, 'update'])->name('teacher.my-course.update');
-        // Route::get('/delete/{id}',[CourseController::class, 'destroy'])->name('teacher.my-course.delete');
+        Route::post('/update/{id}',[TeacherController::class, 'updateSlot'])->name('teacher.my-slot.update');
+        Route::get('/delete/{id}',[TeacherController::class, 'deleteSlot'])->name('teacher.my-slot.delete');
     });
 
 ?>
